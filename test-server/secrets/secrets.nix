dbfab979463c38a735ea9042c2e15ab4c7ea4291
Kopatz <7265381+Kropatz@users.noreply.github.com>
2023-10-26 17:50:10 +0200

    rekey secrets

--- test-server/secrets/secrets.nix
+++ test-server/secrets/secrets.nix
@@ -1,11 +1,12 @@
 let
   nix-test-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMVqEb1U1c9UX3AF8otNyYKpIUMjc7XSjZY3IkIPGOqi root@server";
-  systems = [ nix-test-vm ];
+  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAUA7uVKXAF2UcwaIDSJP2Te8Fi++2zkKzSPoRx1vQrI root@server";
+  systems = [ nix-test-vm server ];
 in
 {
-  "github-runner-token.age".publicKeys = [ nix-test-vm ];
-  "github-runner-pw.age".publicKeys = [ nix-test-vm ];
-  "duckdns.age".publicKeys = [ nix-test-vm ];
-  "nextcloud-admin.age".publicKeys = [ nix-test-vm ];
-  "restic-pw.age".publicKeys = [ nix-test-vm ];
+  "github-runner-token.age".publicKeys = [ nix-test-vm server ];
+  "github-runner-pw.age".publicKeys = [ nix-test-vm server ];
+  "duckdns.age".publicKeys = [ nix-test-vm server ];
+  "nextcloud-admin.age".publicKeys = [ nix-test-vm server ];
+  "restic-pw.age".publicKeys = [ nix-test-vm server ];
 }
