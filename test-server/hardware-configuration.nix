ff47f6f13177ec40dae6a1cd29d9a4063041322d
Kopatz <7265381+Kropatz@users.noreply.github.com>
2023-10-26 20:02:09 +0200

    add disks

--- test-server/hardware-configuration.nix
+++ test-server/hardware-configuration.nix
@@ -27,6 +27,21 @@
     [ { device = "/dev/disk/by-uuid/e4bf3959-4d3c-4980-82c1-c5ec2104ec93"; }
     ];
 
+  fileSystems."/mnt/2tb" =
+    { device = "/dev/disk/by-uuid/99954059-3801-4abb-a536-0e7802a3e6b4";
+      fsType = "ext4";
+    };
+  
+  fileSystems."/mnt/1tb" =
+    { device = "/dev/disk/by-uuid/fb0a94c2-95df-4f62-904e-695d372363e9";
+      fsType = "ext4";
+    };
+  
+  fileSystems."/mnt/250ssd" =
+    { device = "/dev/disk/by-uuid/80163cf9-2030-4757-ada2-03db96184961";
+      fsType = "ext4";
+    };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
