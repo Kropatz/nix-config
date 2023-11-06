805d62549159d2a6b952f2385a324b1c9d26c07d
bayram-budak <byabdk@icloud.com>
2023-11-06 22:41:36 +0100

    first attempt at adding ente public

--- server/modules/nginx.nix
+++ server/modules/nginx.nix
@@ -52,6 +52,34 @@
                     '';
                     proxyPass = "http://127.0.0.1:8080";
                 };
+            },
+            #discord bot for tracking useractivity public version 
+            "activitytracker.site" = {
+                root = "/data/website";
+                forceSSL = true;
+                enableACME = true;
+                locations."~* \\.(jpg)$".extraConfig= ''
+                    add_header Access-Control-Allow-Origin *;
+                '';
+                locations."~ ^/(stash|resources|css)".extraConfig=''
+                	client_max_body_size    5000M;
+                	proxy_redirect          off;
+                	proxy_set_header        Host $host;
+                	proxy_set_header        X-Real-IP $remote_addr;
+                	proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+                	proxy_set_header        X-Forwarded-Proto $scheme;
+                	proxy_set_header        X-NginX-Proxy true;
+                	proxy_pass http://localhost:5092;
+                '';
+		        locations."/" = {
+			        tryFiles = "$uri $uri/ /tracker-site/index.html =404";
+		        };
+                locations."/api" = {
+                    extraConfig =''
+                        rewrite /api/(.*) /$1 break;
+                    '';
+                    proxyPass = "http://127.0.0.1:8080";
+                };
             };
             "adguard.local" = {
                 locations."/".proxyPass = "http://127.0.0.1:3000";
