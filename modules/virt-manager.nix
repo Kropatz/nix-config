2058e0fa5339341c3e4bf33df0675b112f45c64d
Kopatz <7265381+Kropatz@users.noreply.github.com>
2023-12-03 18:14:04 +0100

    update virt manager

--- modules/virt-manager.nix
+++ modules/virt-manager.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 {
-  virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true; # virt-manager requires dconf to remember settings
-  environment.systemPackages = with pkgs; [ virt-manager virtiofsd virtio-win ];
+  environment.systemPackages = with pkgs; [ virt-manager virtiofsd ];
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;  
 }
