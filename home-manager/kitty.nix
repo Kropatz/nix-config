8faf7664b89cd548b1f6abfce4374a493bde7aea
Kopatz <7265381+Kropatz@users.noreply.github.com>
2023-12-17 17:47:45 +0100

    move laptop files

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -5,16 +5,18 @@
     settings = {
       foreground = "#${config.colorScheme.colors.base05}";
       background = "#${config.colorScheme.colors.base00}";
+
+      active_tab_foreground = "#${config.colorScheme.colors.base05}";
+      active_tab_background = "#${config.colorScheme.colors.base01}";
+      inactive_tab_foreground = "#${config.colorScheme.colors.base05}";
+      inactive_tab_background = "#${config.colorScheme.colors.base00}";
       env = "TERM=xterm-256color";
       background_opacity = "0.5";
       font_size = 13;
       # ...
     };
     extraConfig = ''
-      map ctrl+left neighboring_window left
-      map shift+left move_window right
-      map ctrl+down neighboring_window down
-      map shift+down move_window up
+      map ctrl+shift+t new_tab_with_cwd
     '';
   };
 }
