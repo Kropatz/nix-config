b9e987dda92db1c944171930fa140827ccd5d58b
Kopatz <7265381+Kropatz@users.noreply.github.com>
2023-12-17 17:19:36 +0100

    set opacity and font size

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -6,6 +6,8 @@
       foreground = "#${config.colorScheme.colors.base05}";
       background = "#${config.colorScheme.colors.base00}";
       env = "TERM=xterm-256color";
+      background_opacity = "0.5";
+      font_size = 13;
       # ...
     };
     extraConfig = ''
