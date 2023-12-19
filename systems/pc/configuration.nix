73c333839d5d24e14678b8283ca161d79f0859c1
Kopatz <7265381+Kropatz@users.noreply.github.com>
2023-12-19 14:08:11 +0100

    sounds cuts out sometimes, idk maybe this fixes it

--- systems/pc/configuration.nix
+++ systems/pc/configuration.nix
@@ -29,6 +29,7 @@
   nix.settings.trusted-substituters = [ "https://ai.cachix.org" ];
   nix.settings.trusted-public-keys = [ "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc=" ];
   
+  services.nscd.enableNsncd = false;
   #disable firewall when doing ipv6 vm stuff
   #networking.firewall.enable = lib.mkForce false;
 
