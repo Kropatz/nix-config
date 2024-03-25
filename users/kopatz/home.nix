3c20bf7d3ef9c52abd8d31ece91200f4867511f5
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-03-25 13:15:47 +0100

    fail

--- users/kopatz/home.nix
+++ users/kopatz/home.nix
@@ -15,6 +15,7 @@
   imports = [
     ../../home-manager/nvim.nix
     ../../home-manager/code.nix
+    #../../home-manager/browser.nix extensions dont work with ungoogled chromium sadly
     ../../home-manager/zsh.nix
     ../../home-manager/gtk-theme.nix
     ../../home-manager/direnv.nix
