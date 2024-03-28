34a48d99e67e5cc0f71742ba4473e7d1e8dc63cb
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-03-28 19:23:04 +0100

    todo

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -11,6 +11,7 @@
           updateOffline = "sudo nixos-rebuild switch --option substitute false";
           checkTime = "(cd ~/Nextcloud/work_drive/TS && nix run)";
           checkWaylandWindowsKDE = "qdbus org.kde.KWin /KWin org.kde.KWin.showDebugConsole";
+          # TODO: gifsicle -O3 --lossy=30 noita-20240328-191617-1612416266-00316616.gif -o noita-20240328-191617-1612416266.gif 
           ssh = "TERM=xterm-256color ssh";
       };
       oh-my-zsh = {
