8907b6418f5596538f3c1a117d450e5f5ce0e691
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-03-31 17:52:33 +0200

    add comments

--- modules/services/gitolite.nix
+++ modules/services/gitolite.nix
@@ -1,5 +1,7 @@
 { config, ...}:
 {
+  # configure git clone gitolite@server:gitolite-admin
+  # help ssh gitolite@server help
   services.gitolite = {
     enable = true;
     adminPubkey = config.mainUser.sshKey;
