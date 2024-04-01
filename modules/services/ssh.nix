018b3e8f733fa4caee0fc0df72b258d7fa5e6752
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-04-01 19:46:54 +0200

    adapt ssh config

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -2,9 +2,15 @@
     networking.firewall.allowedTCPPorts = [ 22 ];
     services.openssh = {
         enable = true;
+        allowSFTP = false;
         settings.PasswordAuthentication = false;
         settings.KbdInteractiveAuthentication = false;
+        settings.X11Forwarding = false;
         settings.PermitRootLogin = "no";
-        settings.X11Forwarding = true;
+        extraConfig = ''
+            AllowAgentForwarding no
+            AllowStreamLocalForwarding no
+            AuthenticationMethods publickey
+        '';
     };
 }
