3ed4d6262ced66c80e3917b3573f60a50642164d
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-04-01 20:11:00 +0200

    change some docker stuff

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, inputs, ... }:
 {
     virtualisation.docker.enable = true;
+    virtualisation.docker.daemon.settings = { ip = "127.0.0.1"; };
     environment.systemPackages = with pkgs; [
         docker-compose
     ];
