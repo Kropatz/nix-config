94fa0540fc84928bcf961cb4f17ba2db44e4aede
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-04-26 13:41:54 +0200

    add allow unfree environment var

--- modules/misc/hook.sh
+++ modules/misc/hook.sh
@@ -10,6 +10,7 @@ STATE_NAME="$3"
 GPU_PCI="pci_0000_2b_00_0"
 GPU_AUDIO_PCI="pci_0000_2b_00_1"
 #GPU_MODULES="nvidia_uvm nvidia_drm nvidia_modeset nvidia"
+set -x
 
 if [[ ! "$GUEST_NAME" =~ ^gpu_passthrough ]]; then
     exit 0
@@ -22,12 +23,14 @@ if [ "$HOOK_NAME" == "prepare" ] && [ "$STATE_NAME" == "begin" ]; then
     echo 0 > /sys/class/vtconsole/vtcon0/bind
     echo 0 > /sys/class/vtconsole/vtcon1/bind
     echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+    
+    sleep 2
 
     # Unload kernel module
-    modprobe -r nvidia_uvm
-    modprobe -r nvidia_drm
-    modprobe -r nvidia_modeset
-    modprobe -r nvidia
+    rmmod nvidia_uvm
+    rmmod nvidia_drm
+    rmmod nvidia_modeset
+    rmmod nvidia
 
     # Detach GPU from host
     virsh nodedev-detach $GPU_PCI
@@ -37,13 +40,12 @@ if [ "$HOOK_NAME" == "prepare" ] && [ "$STATE_NAME" == "begin" ]; then
     modprobe vfio-pci
 elif [ "$HOOK_NAME" == "release" ] && [ "$STATE_NAME" == "end" ]; then
     # Unload VFIO kernel modules
-    modprobe -r vfio-pci
+    #modprobe -r vfio-pci
 
     # Reattach GPU TO host
     virsh nodedev-reattach $GPU_PCI
     virsh nodedev-reattach $GPU_AUDIO_PCI
 
-    echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
 
     # Load NVIDIA kernel modules
     modprobe nvidia
@@ -53,6 +55,7 @@ elif [ "$HOOK_NAME" == "release" ] && [ "$STATE_NAME" == "end" ]; then
 
     echo 1 > /sys/class/vtconsole/vtcon0/bind
     echo 1 > /sys/class/vtconsole/vtcon1/bind
+    echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
     # Start DM
     systemctl start display-manager.service
 fi
