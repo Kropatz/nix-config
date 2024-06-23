65bdefdb9d20ce7082f756a0827ab7cda1ae4bd0
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-06-23 11:59:33 +0200

    add eza

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -13,6 +13,8 @@
       collectGarbage = "nh clean all";
       edit = "cd ~/projects/github/nix-config && nvim .";
       ll = "ls -l";
+      ls = "eza --icons";
+      la = "eza -la --icons --group-directories-first";
       ssh = "TERM=xterm-256color ssh";
       update = "sudo nixos-rebuild switch";
       updateFancy = "nh os switch";
