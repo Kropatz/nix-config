1c3f87f68e805351a4708772f5f90174567e0c7e
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-09-07 11:50:08 +0200

    remove overlays

--- overlays.nix
+++ overlays.nix
@@ -11,26 +11,26 @@
     #discord = prev.vesktop;
     tetrio-desktop = prev.tetrio-desktop.override { withTetrioPlus = true; };
     nerdfonts = prev.nerdfonts.override { fonts = [ "Hack" "Noto" ]; };
-    waybar = prev.waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-    });
-    hyprland =
-      inputs.hyprland.packages.${prev.stdenv.hostPlatform.system}.hyprland;
-    xdg-desktop-portal-hyprland =
-      inputs.hyprland.packages.${prev.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    delta = prev.delta.overrideAttrs (oldAttrs: rec {
-      version = "0.17.0-unstable-2024-08-12";
-      src = prev.fetchFromGitHub {
-        owner = "dandavison";
-        repo = "delta";
-        rev = "a01141b72001f4c630d77cf5274267d7638851e4";
-        hash = "sha256-My51pQw5a2Y2VTu39MmnjGfmCavg8pFqOmOntUildS0=";
-      };
-      cargoDeps = oldAttrs.cargoDeps.overrideAttrs {
-        inherit src;
-        outputHash = "sha256-TJ/yLt53hKElylycUfGV8JGt7GzqSnIO3ImhZvhVQu0=";
-      };
-    });
+    #waybar = prev.waybar.overrideAttrs (oldAttrs: {
+    #  mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    #});
+    #hyprland =
+    #  inputs.hyprland.packages.${prev.stdenv.hostPlatform.system}.hyprland;
+    #xdg-desktop-portal-hyprland =
+    #  inputs.hyprland.packages.${prev.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    #delta = prev.delta.overrideAttrs (oldAttrs: rec {
+    #  version = "0.17.0-unstable-2024-08-12";
+    #  src = prev.fetchFromGitHub {
+    #    owner = "dandavison";
+    #    repo = "delta";
+    #    rev = "a01141b72001f4c630d77cf5274267d7638851e4";
+    #    hash = "sha256-My51pQw5a2Y2VTu39MmnjGfmCavg8pFqOmOntUildS0=";
+    #  };
+    #  cargoDeps = oldAttrs.cargoDeps.overrideAttrs {
+    #    inherit src;
+    #    outputHash = "sha256-TJ/yLt53hKElylycUfGV8JGt7GzqSnIO3ImhZvhVQu0=";
+    #  };
+    #});
 
     # .png doesnt work :(
     #fastfetch = prev.fastfetch.overrideAttrs (oldAttrs: {
