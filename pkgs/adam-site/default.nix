2123fa7682f3a3a7f5e66138cb3ca5aa04009ac4
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-10-05 11:28:02 +0200

    update adam

--- pkgs/adam-site/default.nix
+++ pkgs/adam-site/default.nix
@@ -6,7 +6,7 @@ buildNpmPackage rec {
   src = fetchGit {
     url = "git@github.com:oberprofis/adams.git";
     ref = "main";
-    rev = "921d3deff4d4777b6948e642ae5f27d43b1e2e26";
+    rev = "2bb42879fc9cf94217b0759b5c09a63ff69f5bac";
   };
   npmDepsHash="sha256-PRFHBlVIdHfATAAKVKax+bY4o+9czdfl7HjFnKk4KtI=";
   installPhase = ''
