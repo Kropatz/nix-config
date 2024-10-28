94ec756fd5a7fe9c7e165f6a928b3f949763efcb
Kopatz <7265381+Kropatz@users.noreply.github.com>
2024-10-28 09:54:17 +0100

    add page size patch

--- modules/services/kavita.nix
+++ modules/services/kavita.nix
@@ -50,7 +50,7 @@ in {
       user = "kavita";
       package = let
         backend = pkgs.unstable.kavita.backend.overrideAttrs
-          (old: { patches = old.patches ++ [ ./kavita-patches-chapter-parsing.diff]; });
+          (old: { patches = old.patches ++ [ ./kavita-patches-chapter-parsing.diff ./kavita-page-size.diff ]; });
         kavitaPatched = pkgs.unstable.kavita.overrideAttrs (old: { backend = backend; });
       in kavitaPatched;
       settings = {
