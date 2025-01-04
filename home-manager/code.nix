46d8a1c5ebaf1a43de5dfb979325428e34b70c8b
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-01-04 19:49:21 +0100

    add typst extension

--- home-manager/code.nix
+++ home-manager/code.nix
@@ -3,9 +3,13 @@
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
+      userSettings = { typst-lsp.exportPdf = "onType"; };
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
         rust-lang.rust-analyzer
+        nvarner.typst-lsp
+        #tomoki1207.pdf latex-workshop is faster to preview pdf
+        james-yu.latex-workshop
       ];
     };
   };
