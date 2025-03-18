923ab372096dece52ea60bdca9438d269171ac43
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-03-18 12:48:26 +0100

    set monitor in dunst

--- home-manager/dunst.nix
+++ home-manager/dunst.nix
@@ -13,6 +13,7 @@ in {
           transparency = -1;
           frame_color = "#1a1c1b";
           font = lib.mkDefault "Monospace 8";
+          monitor = 1;
         };
 
         urgency_normal = {
