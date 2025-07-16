430bb5d53215f2fdaff1c73f1013d30f816a1b53
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-07-16 19:50:12 +0200

    hop

--- home-manager/nixvim/config.nix
+++ home-manager/nixvim/config.nix
@@ -190,6 +190,32 @@
       key = "<leader>rI";
       action = ":Refactor inline_func ";
       options.desc = "Inline function";
+    } 
+    # hop
+        {
+      key = "f";
+      action.__raw = ''
+        function()
+          require'hop'.hint_char1({
+            --direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
+            current_line_only = false,
+            case_insensitive = true,
+          })
+       end
+      '';
+      options.remap = true;
+    }
+    {
+      key = "F";
+      action.__raw = ''
+        function()
+          require'hop'.hint_char1({
+            direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
+            current_line_only = false
+          })
+        end
+     '';
+      options.remap = true;
     }
   ];
 }
--- home-manager/nixvim/hop.nix
+++ home-manager/nixvim/hop.nix
@@ -2,31 +2,5 @@
   plugins.hop = {
     enable = true;
   };
-  keymaps = [
-    {
-      key = "f";
-      action.__raw = ''
-        function()
-          require'hop'.hint_char1({
-            --direction = require'hop.hint'.HintDirection.AFTER_CURSOR,
-            current_line_only = false,
-            case_insensitive = true,
-          })
-       end
-      '';
-      options.remap = true;
-    }
-    {
-      key = "F";
-      action.__raw = ''
-        function()
-          require'hop'.hint_char1({
-            direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,
-            current_line_only = false
-          })
-        end
-     '';
-      options.remap = true;
-    }
-  ];
+  # keybinds moved to config.nix, my merging method sux and having multiple lists overrides the other
 }
