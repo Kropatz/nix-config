73c4890c5f29a8aaa0b23a78a2eba82d9261335a
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-07-29 18:33:34 +0200

    disable ts for large files

--- home-manager/nixvim/treesitter.nix
+++ home-manager/nixvim/treesitter.nix
@@ -5,7 +5,18 @@
       nixGrammars = true;
       settings = {
         indent.enable = true;
-        highlight.enable = true;
+        highlight = {
+          enable = true;
+          disable = ''
+            function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end
+          '';
+        };
       };
     };
     #treesitter-context.enable = true;
