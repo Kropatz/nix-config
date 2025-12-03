5897ab0196a52184b9e9e7facbe953be9df4abcd
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-12-03 15:42:49 +0100

    add pathj

--- home-manager/hyprland/hyprland-settings.nix
+++ home-manager/hyprland/hyprland-settings.nix
@@ -434,8 +434,9 @@ in
           bind = $mainMod, W, exec, ${wl-paste} | ${answer} "Respond with the correct answer to the following question." | ${wl-copy} && ${dunstify} -t 150 Done
           ## notes
 
+          bind = $mainMod, 1, exec, cat ~/synced/default/vimwiki/fh/master/incident_handling/pruefung.md | ${wl-copy}
           bind = $mainMod, 2, exec, cat ~/synced/default/vimwiki/fh/master/incident_handling/a.md | ${wl-copy}
-          bind = $mainMod, 3, exec, cat ~/synced/default/vimwiki/fh/master/incident_handling/pruefung.md | ${wl-copy}
+          bind = $mainMod, 3, exec, cat ~/synced/default/vimwiki/fh/master/incident_handling/events.md | ${wl-copy}
           #bind = $mainMod, 4, exec, cat ~/Nextcloud/test.txt | ${wl-copy}
           #bind = $mainMod, 5, exec, cat ~/Nextcloud/test.txt | ${wl-copy}
           #bind = $mainMod, 6, exec, cat ~/Nextcloud/test.txt | ${wl-copy}
