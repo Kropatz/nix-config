e8ab4e200377fddf5de1caf4be504e11caca08cb
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-12-05 17:52:48 +0100

    list home manager pkgs too

--- modules/misc/packages-list.nix
+++ modules/misc/packages-list.nix
@@ -3,7 +3,8 @@
   environment.etc."current-system-packages".text =
     let
       packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
-      sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+      homePackages = builtins.map (p: "${p.name}") config.home-manager.users.${config.mainUser.name}.home.packages;
+      sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique (packages ++ homePackages));
       formatted = builtins.concatStringsSep "\n" sortedUnique;
     in
     formatted;
