9d818b4c34abdcff0028ff644015fbee0d41ba0d
Kopatz <7265381+Kropatz@users.noreply.github.com>
2026-03-15 13:39:43 +0100

    update adam site url

--- pkgs/adam-site/default.nix
+++ pkgs/adam-site/default.nix
@@ -8,7 +8,7 @@ buildNpmPackage rec {
   version = "1.0.0";
 
   src = fetchGit {
-    url = "git@github.com:oberprofis/adams.git";
+    url = "gitea@git.kopatz.dev:oberprofis/adams.git";
     ref = "main";
     rev = "6575c418f45aef025d2d89d5b0b4ff4fbdffe298";
   };
